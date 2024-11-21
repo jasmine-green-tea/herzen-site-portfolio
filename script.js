2de62f22910beb6af9169756112a0f0ab633cb81
jasmine-green-tea <shelldary@yandex.ru>
2024-11-21 22:22:30 +0300

    artwork

---
+++ script.js
@@ -0,0 +1,52 @@
+let sliderImages = document.querySelectorAll(".slide"),
+    arrowLeft = document.querySelector("#arrow-left"),
+    arrowRight = document.querySelector("#arrow-right"),
+    current = 0;
+
+    function reset()
+    {
+        for(let i = 0; i < sliderImages.length; i++)
+        {
+            sliderImages[i].style.display = "none";
+        }
+    }
+
+    function startSlide()
+    {
+        reset();
+        sliderImages[0].style.display = "block";
+    }
+
+    function sliderLeft()
+    {
+        reset();
+        sliderImages[current-1].style.display = "block";
+        current--;
+    }
+
+    function sliderRight()
+    {
+        reset();
+        sliderImages[current+1].style.display = "block";
+        current++;
+    }
+
+    arrowLeft.addEventListener("click", function()
+    {
+        if (current === 0)
+        {
+            current = sliderImages.length;
+        }
+        sliderLeft();
+    });
+
+    arrowRight.addEventListener("click", function()
+    {
+        if (current === sliderImages.length - 1)
+        {
+            current = -1;
+        }
+        sliderRight();
+    });
+
+    startSlide();
